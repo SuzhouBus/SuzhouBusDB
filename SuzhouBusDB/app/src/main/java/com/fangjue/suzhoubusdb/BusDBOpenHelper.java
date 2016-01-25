f93792bf8e56e6ae372399e6052cf2d4db35e16c
方觉 <fangjue23303@gmail.com>
2016-01-25 20:09:47 +0800

    Initial work on bus categories, with UI skeleton and database initialization.

--- SuzhouBusDB/app/src/main/java/com/fangjue/suzhoubusdb/BusDBOpenHelper.java
+++ SuzhouBusDB/app/src/main/java/com/fangjue/suzhoubusdb/BusDBOpenHelper.java
@@ -5,7 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class BusDBOpenHelper extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
     private static final String DATABASE_NAME_PREFIX = "BUSDB_";
     public static final String kTableBuses = "buses";
     public static final String kBusId = "busId";
@@ -29,23 +29,42 @@ public class BusDBOpenHelper extends SQLiteOpenHelper {
             kLineId + " VARCHAR(32) NOT NULL, " +
             kCompanyId + " VARCHAR(1) NOT NULL );";
 
+    // Version 3: Add bus categories table.
+    public static final String kTableCategories = "categories";
+    public static final String kCategoryName = "name";
+    public static final String kBusIdMin = "busIdMin";
+    public static final String kBusIdMax = "busIdMax";
+    private static final String CATEGORIES_TABLE_CREATE =
+            "CREATE TABLE " + kTableCategories + " (" +
+            kCategoryName + " VARCHAR(256) NOT NULL, " +
+            kBusIdMin + " VARCHAR(16) NOT NULL, " +
+            kBusIdMax + " VARCHAR(16) NOT NULL );";
+
     BusDBOpenHelper(Context context, String city) {
         super(context, DATABASE_NAME_PREFIX + city, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(BUSES_TABLE_CREATE + LINES_TABLE_CREATE);
+        db.execSQL(BUSES_TABLE_CREATE +
+                   LINES_TABLE_CREATE +
+                   CATEGORIES_TABLE_CREATE
+        );
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        if (newVersion > 2)
+        if (newVersion > 3)
             throw new UnsupportedOperationException();
 
-        if (oldVersion == 1) {
+        if (oldVersion <= 1) {
             // Upgrade from Version 1 to 2.
             db.execSQL(LINES_TABLE_CREATE);
+        }
+
+        if (oldVersion <= 2) {
+            // Upgrade from Version 2 to 3.
+            db.execSQL(CATEGORIES_TABLE_CREATE);
         }
     }
 }
